d014309e79416142eab5f19b79c8628414544970
ArchanaManju <archanahm2021@gmail.com>
2023-07-10 16:39:50 +0100

    sec commit

--- app.js
+++ app.js
@@ -5,8 +5,7 @@
  * <h2> I am h2 tag </h2>
  * </div>
  * </div>
- * 
- */
+*/
 const parent = React.createElement("div",{id:"parent"}, React.createElement("div",{id:"child"},[React.createElement("h1",{}, "i am h1 tag"),React
 .createElement("h2",{}, "I am h2 tag")]))
 const heading = React.createElement("h1", {id:"heading", xyz:"abc"}, "hello world from React")
